b40d71619836710f1d1a590bcc5d6042176bc036
Alexander Cahlin <tna13aca@student.lu.se>
2018-01-18 17:26:41 +0100

    moved files to within my User folder

---
+++ nrn/src/nrnoc/nrnconfigargs.h
@@ -0,0 +1,8 @@
+/* src/nrnoc/nrnconfigargs.h.  Generated from nrnconfigargs.h.in by configure.  */
+#ifndef nrnconfigargs_h
+#define nrnconfigargs_h
+
+/* define to the args passed to configure */
+#define NRN_CONFIG_ARGS " '--prefix=/home/hines/neuron/nrnwinobj' '-with-nrnpython=dynamic' '--with-pyexe=/e/python36/python' '--host=x86_64-w64-mingw32' '--with-paranrn=dynamic' 'MPICC=x86_64-w64-mingw32-gcc' 'MPICXX=x86_64-w64-mingw32-g++' 'CFLAGS=-DMSMPI_NO_SAL -I/c/ms-mpi/include' 'LIBS=-L/c/ms-mpi/lib/x64 -lmsmpi' 'PYINCDIR=/e/python36/include' 'PYLIBDIR=/e/python36/libs' 'PYLIB=-L/e/python36/libs -lpython36' 'PYLIBLINK=-L/e/python36/libs -lpython36' 'MPILIBS=-L/c/ms-mpi/lib/x64 -lmsmpi' 'host_alias=x86_64-w64-mingw32' 'PKG_CONFIG_PATH=/mingw64/lib/pkgconfig:/mingw64/share/pkgconfig'"
+
+#endif
---
+++ nrn/src/nrnoc/nrnversion.h
@@ -0,0 +1,4 @@
+#define GIT_DATE "2017-09-25"
+#define GIT_BRANCH "master"
+#define GIT_CHANGESET "6b4c19f"
+#define GIT_LOCAL "1650"
